0d580861a456ef227cc5bc3aeb46542b5e445966
danzafar <dan.zafar@databricks.com>
2020-02-23 16:54:07 -0700

    got n(), and indirectly tall() and count() working

--- R/columns.R
+++ R/columns.R
@@ -1,15 +1,3 @@
-# How can we avoid a namespace conflict for n()?
-#' @export
-n <- function() {
-  group_size <- dplyr:::context_env[["..group_size"]]
-  if (!is.null(group_size)) {
-    group_size
-  } else {
-    jc <- SparkR:::callJStatic("org.apache.spark.sql.functions", "count", "*")
-    SparkR:::column(jc)
-  }
-}
-
 other_functions <- c("like", "rlike", "getField", "getItem", "contains", "asc")
 
 for (.f in other_functions) {
