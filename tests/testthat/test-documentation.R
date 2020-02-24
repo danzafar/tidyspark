e17861b1dc58e3d0c43374298cd89119226c38c5
danzafar <dan.zafar@databricks.com>
2020-02-24 08:15:23 -0700

    updated tests, minor correction in filter

---
+++ tests/testthat/test-documentation.R
@@ -0,0 +1,38 @@
+
+library(dplyr)
+flights_tbl <- spark_tbl(nycflights13::flights)
+# batting_tbl <- spark_tbl(Lahman::Batting)
+
+test_that("simple filter", {
+  expect_equal(flights_tbl %>% filter(dep_delay == 2) %>% collect %>% select(1:5) %>% as.list,
+               nycflights13::flights %>% filter(dep_delay == 2) %>% select(1:5) %>% as.list)
+})
+
+test_that("group_by, summarise, filter on flights_tbl", {
+  expect_equal(flights_tbl %>%
+                 group_by(tailnum) %>%
+                 summarise(count = n(),
+                           dist = mean(distance),
+                           delay = mean(arr_delay)) %>%
+                 filter(count > 20, dist < 2000, !is.na(delay)) %>%
+                 collect %>%
+                 arrange(tailnum) %>%
+                 mutate(delay = round(delay, 16)),
+               nycflights13::flights %>%
+                 group_by(tailnum) %>%
+                 summarise(count = as.numeric(n()),
+                           dist = mean(distance),
+                           delay = round(mean(arr_delay, na.rm = T), 16)) %>%
+                 filter(count > 20, dist < 2000, !is.na(delay)) %>%
+                 arrange(tailnum)
+               )
+})
+
+test_that("group_by, summarise, filter on flights_tbl", {
+  spark_tbl(iris) %>% register_temp_view("iris")
+  iris_preview <- spark_sql("SELECT * FROM iris LIMIT 10")
+  expect_equal(iris_preview %>% collect,
+               iris %>% setNames(names(iris) %>% sub("[//.]", "_", .)) %>%
+                 mutate(Species = levels(Species)[Species]) %>%
+                 head(10))
+})
