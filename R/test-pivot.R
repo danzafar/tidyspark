848ea85d2a92dbf70f62879e0ff0b74c24c6fc55
danzafar <dan.zafar@databricks.com>
2020-03-02 10:26:19 -0700

    clean up some disrupting test code

--- R/test-pivot.R
+++ R/test-pivot.R
@@ -1,13 +1,13 @@
-library(testthat)
-library(dplyr)
-
-mtcars_spk <- data.frame(A = c("G", "H"),
-                         X = c(4, 1),
-                         Y = c(20, 40),
-                         Z = c(200, 400)) %>%
-  spark_tbl() %>%
-  piv_longer(cols = c(X,Y,Z), names_to = "name_stuff", values_to = "number_stuff")
-
-
-mtcars %>%
-  tidyr::pivot_longer(cols = c(mpg, cyl), names_to = "name_stuff", values_to = "number_stuff")
+# library(testthat)
+# library(dplyr)
+#
+# mtcars_spk <- data.frame(A = c("G", "H"),
+#                          X = c(4, 1),
+#                          Y = c(20, 40),
+#                          Z = c(200, 400)) %>%
+#   spark_tbl() %>%
+#   piv_longer(cols = c(X,Y,Z), names_to = "name_stuff", values_to = "number_stuff")
+#
+#
+# mtcars %>%
+#   tidyr::pivot_longer(cols = c(mpg, cyl), names_to = "name_stuff", values_to = "number_stuff")
