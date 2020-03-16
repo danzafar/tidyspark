1ab5f73fb97b9c32e17e7b1338f850705cf62c01
danzafar <dan.zafar@databricks.com>
2020-03-16 16:40:26 -0600

    updated documentation, added function to get spark session

--- R/spark.R
+++ R/spark.R
@@ -17,34 +17,62 @@ spark_session_reset <- function() {
   SparkR:::sparkR.session()
 }
 
-#' Title
+#' Get Spark Session
 #'
-#' @param expr
-#' @param ...
+#' @return a \code{jobj} of the spark session
+#' @export
+#'
+#' @examples
+#' get_spark_session()
+get_spark_session <- function() {
+  if (exists(".sparkRsession", envir = SparkR:::.sparkREnv)) {
+    get(".sparkRsession", envir = SparkR:::.sparkREnv)
+  }
+  else {
+    stop("SparkSession not initialized")
+  }
+}
+
+#' Spark SQL
+#'
+#' @description Execute Spark SQL queries directly on the spark session
+#'
+#' @param expr a string, the SQL query
+#'
+#' @return a \code{spark_tbl}
+#' @export
+#'
+#' @examples
+#' spark_tbl(iris) %>% register_temp_view("iris")
+#' iris_preview <- spark_sql("SELECT * FROM iris LIMIT 10")
+#' iris_preview %>% collect
+spark_sql <- function(expr) {
+  sdf <- SparkR:::callJMethod(get_spark_session(), "sql", expr)
+  new_spark_tbl(new("SparkDataFrame", sdf, F))
+  }
+
+#' Create or replace a temporary view
+#'
+#' @description similar to Spark's \code{createOrReplaceTemporaryView} method,
+#' this function registers the DAG at a given point in the lineage as a temp view
+#' in the hive metastore. It does not cache the data, but the name supplied can
+#' be used in future Spark SQL queries.
+#'
+#' @param .data a \code{spark_tbl} to be registered
+#' @param name a \code{string} of the name to store the table as
 #'
 #' @return
 #' @export
 #'
 #' @examples
-spark_sql <- function(expr, ...) {
-  sdf <- SparkR::sql(expr, ...)
-  new_spark_tbl(sdf)
+#' #' spark_tbl(iris) %>% register_temp_view("iris")
+#' iris_preview <- spark_sql("SELECT * FROM iris LIMIT 10")
+#' iris_preview %>% collect
+register_temp_view <- function(.data, name) {
+  sdf <- attr(.data, "DataFrame")
+  SparkR:::callJMethod(sdf@sdf, "createOrReplaceTempView", name)
+  invisible()
   }
-
-#' Title
-#'
-#' @param sdf
-#' @param name
-#'
-#' @return
-#' @export
-#'
-#' @examples
-register_temp_view <- function(sdf, name) {
-  sdf <- attr(sdf, "DataFrame")
-  SparkR::createOrReplaceTempView(sdf, name)
-  }
-
 
 #' Get Spark Class
 #'
