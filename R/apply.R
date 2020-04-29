0f42061400f7bd4a44b52e8bf09f3bdfa317ba6e
danzafar <dan.zafar@databricks.com>
2020-04-29 09:15:03 -0600

    got spark_lapply working

--- R/apply.R
+++ R/apply.R
@@ -174,10 +174,36 @@ spark_grouped_udf <- function (.data, .f, schema, cols = NULL) {
   new_spark_tbl(sdf)
 }
 
-# # spark.lapply ---------
-# spark_lapply <- function (list, func) {
-#   sc <- SparkR:::getSparkContext()
-#   rdd <- SparkR:::parallelize(sc, list, length(list))
-#   results <- SparkR:::map(rdd, func)
-#   SparkR:::collectRDD(results)
-# }
+#' Apply a Function over a List or Vector, Distribute operations in Spark
+#'
+#' @description Run a function over a list of elements, distributing the
+#' computations with Spark. Applies a function in a manner that is similar to
+#' doParallel or lapply to elements of a list. The computations are distributed
+#' using Spark. It is conceptually the same as the following
+#' code: \code{lapply(list, func)}
+#'
+#' @param .l a vector (atomic or list)
+#' @param .f he function to be applied to each element of \code{.l}
+#'
+#' @return an in-memory \code{list} object
+#' @export
+#'
+#' @examples
+#'
+#' spark_session()
+#' doubled <- spark_lapply(1:10, function(x) {2 * x})
+#'
+#' # or using tidyverse style lamdas
+#' doubled <- spark_lapply(1:10, ~ 2 * .)
+#'
+spark_lapply <- function (.l, .f) {
+  if (rlang::is_formula(.f)) {
+    .f <- rlang::as_function(.f)
+    .f <- unclass(.f)
+  }
+  sc <- SparkR:::getSparkContext()
+  rdd <- SparkR:::parallelize(sc, .l, length(.l))
+  results <- SparkR:::map(rdd, .f)
+  SparkR:::collectRDD(results)
+}
+
