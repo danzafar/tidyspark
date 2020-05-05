b2a72fa285e7b2359ce5ba9895c244ae43647a83
danzafar <dan.zafar@databricks.com>
2020-05-04 22:32:24 -0600

    fix a test

--- R/utils.R
+++ R/utils.R
@@ -5,10 +5,11 @@ num_to_int <- function(num) {
   as.integer(num)
 }
 
-#TODO make recursive (EG max(if_else(foo, fizz, buzz)) will fail this because max is returned)
 check_if_else <- function(x) {
   if (!rlang::is_call(rlang::get_expr(x))) {
     invisible()
+  } else if (is.null(rlang::call_name(x))) {
+    check_if_else(rlang::call_args(x))
   } else if (rlang::call_name(x) == 'if_else') {
     stop('`if_else` is not defined in tidyspark! Consider `ifelse`.')
   } else {
