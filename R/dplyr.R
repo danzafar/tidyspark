b764f8def8e0a1aec33959b6eb29efaed8343598
danzafar <dan.zafar@databricks.com>
2020-05-06 16:24:16 -0600

    moved fix_dot et. al. to a new script.

--- R/dplyr.R
+++ R/dplyr.R
@@ -80,15 +80,17 @@ distinct.spark_tbl <- function(.data, ...) {
 is_agg_expr <- function(col) {
   if (inherits(col, c("character", "numeric", "logical", "integer"))) return(F)
   if (class(col) == "Column") col <- call_method(col@jc, "expr")
-  name <- SparkR:::getClassName.jobj(col)
-  grepl("expressions\\.aggregate", name)
+  name <- spark_class(col)
+  if (grepl("expressions\\.aggregate", name)) T
+  else if (grepl("expressions\\.CaseWhen", name)) F # TODO Handle aggregations
+  else F
 }
 
 # check to see if a column expression is aggregating
 is_wndw_expr <- function(col) {
   if (inherits(col, c("character", "numeric", "logical", "integer"))) return(F)
   if (class(col) == "Column") col <- call_method(col@jc, "expr")
-  name <- SparkR:::getClassName.jobj(col)
+  name <- spark_class(col)
   grepl("expressions\\.WindowExpression", name)
 }
 
@@ -234,162 +236,6 @@ filter.spark_tbl <- function(.data, ..., .preserve = FALSE) {
   sdf <- attr(.data, "jc")
   df_cols <- get_jc_cols(sdf)
 
-  # This is the most complicated part of tidyspark so far. In order to break up
-  # complicated expressions like:
-  # max(Sepal_Length) > 3 & Petal_Width < 4 | max(Petal_Width) > 2 | ...
-  # I use a recursive function to parse through and convert all of the aggregate
-  # terms into new columns. Then I replace that aggregate term into the new term
-  # and run the filter with it.
-
-  # this function replaces an aggregating expression with an actual sdf column
-  # name that is generated with `withColumn`
-  sub_agg_column <- function(col, env) {
-    # incoming env is expected to have namespace for
-    # j, sdf, and to_drop
-    virt <- paste0("agg_col", env$j)
-    env$j <- env$j + 1
-
-    # generate a window, since we will need it
-    groups <- attr(.data, "groups")
-    group_jcols <- lapply(groups, function(col) get_jc_cols(env$sdf)[[col]]@jc)
-    window <- call_static("org.apache.spark.sql.expressions.Window",
-                                   "partitionBy", group_jcols)
-
-    # apply the window
-    wndw <- call_method(col@jc, "over", window)
-    wndw_col <- new("Column", wndw)
-    sdf_jc <- call_method(env$sdf, "withColumn",
-                                   virt,
-                                   wndw_col@jc)
-    env$sdf <- sdf_jc
-    env$to_drop <- c(env$to_drop, virt)
-    new("Column", call_method(env$sdf, "col", virt))
-  }
-
-  # here is what needs to happen in Spark:
-  # val wndw = rank.over(Window.orderBy($"personid").partitionBy($"name"))
-  # val out = df_asProfile.withColumn("col_wndw_1", wndw)
-  sub_wndw_column <- function(col, env) {
-    # incoming env is expected to have namespace for
-    # j, sdf, and to_drop
-    virt <- paste0("wndw_col", env$j)
-    env$j <- env$j + 1
-
-    func_wndw <- chop_wndw(col)
-
-    # add in the partitionBy based on grouping
-    groups <- attr(.data, "groups")
-    group_jcols <- lapply(groups, function(col) get_jc_cols(env$sdf)[[col]]@jc)
-    window <- call_method(func_wndw$wndw, "partitionBy", group_jcols)
-
-    # apply the window over the function
-    wndw_col <- new("Column", call_method(func_wndw$func, "over", window))
-
-    # add the windowed column
-    sdf_jc <- call_method(env$sdf, "withColumn",
-                          virt,
-                          wndw_col@jc)
-    env$sdf <- sdf_jc
-    env$to_drop <- c(env$to_drop, virt)
-    new("Column", call_method(env$sdf, "col", virt))
-  }
-
-  # this recursive function is needed to parse through abiguously large
-  # conditional expressions like a > b & (b < c | f == g) | g < a & a > e
-  # setting rules on order of operations doesn't make sense, instead we
-  # simply leverage the rlang::call_fn command to get the most outer funciton
-  # then step into each arg of that outer function with rlang::call_args
-  fix_dot <- function(dot, env) {
-    # incoming env is expected to have namespace for
-    # j, sdf, and to_drop
-
-    # early return if there is no calling function (single boolean column)
-    if (!rlang::is_call(rlang::get_expr(dot))) return(rlang::quo_text(dot))
-
-    op <- rlang::call_fn(dot)
-    args <- rlang::call_args(dot)
-    if (identical(op, `&`) | identical(op, `&&`)) {
-      paste(fix_dot(args[[1]], env), "&", fix_dot(args[[2]], env))
-    } else if (identical(op, `|`) | identical(op, `||`)) {
-      paste(fix_dot(args[[1]], env), "|", fix_dot(args[[2]], env))
-    } else if (identical(op, `(`)) {
-      paste("(", fix_dot(args[[1]], env), ")")
-    } else if (identical(op, `==`)) {
-      paste(fix_dot(args[[1]], env), "==", fix_dot(args[[2]], env))
-    } else if (identical(op, `any`) | identical(op, `all`)) {
-      # `any` and `all` are aggregate functions and require special treatment
-      quo <- rlang::as_quosure(dot, env = dot_env)
-      col <- rlang::eval_tidy(quo, df_cols)
-
-      str <- call_method(
-        call_method(
-          call_method(
-            call_method(col@jc, "expr"),
-            "children"),
-          "head"),
-        "toString")
-      parsed <- rlang::parse_quo(sub("(-)?(.*)#.*([)])", "\\2\\3", str),
-                       rlang::quo_get_env(quo))
-      paste(fix_dot(parsed, env), "==", fix_dot(TRUE, env))
-
-    } else if (length(rlang::call_args(dot)) == 1) {
-      quo <- rlang::as_quosure(dot, env = dot_env)
-      col <- rlang::eval_tidy(quo, df_cols)
-
-      is_agg <- is_agg_expr(col)
-      is_wndw <- is_wndw_expr(col)
-
-      if (is_agg | is_wndw) {
-        if (is_agg_expr(col)) col <- sub_agg_column(col, env)
-        if (is_wndw_expr(col)) col <- sub_wndw_column(col, env)
-        call_method(col@jc, "toString")
-      } else rlang::quo_text(dot)
-
-    } else {
-      cond <- rlang::eval_tidy(dot, df_cols)
-      and_expr <- call_method(cond@jc, "expr")
-      if (spark_class(and_expr, trunc = T) == "Not") {
-        and_expr <- call_method(
-          call_method(and_expr, "children"),
-          "head")
-      }
-      left <- call_method(and_expr, "left")
-      right <- call_method(and_expr, "right")
-      if (is_agg_expr(left) | is_agg_expr(right)) {
-        # we extract both sides, turn them into quosures that we can do eval_tidy
-        # on separately.
-        pred_func <- rlang::call_fn(dot)
-        args <- rlang::call_args(dot)
-        dot_env <- rlang::quo_get_env(dots[[i]])
-        quos <- rlang::as_quosures(args, env = dot_env)
-        left_col <- rlang::eval_tidy(quos[[1]], df_cols)
-        right_col <- rlang::eval_tidy(quos[[2]], df_cols)
-
-        # Now we need to replace the agg quosure with a virtual column
-        # consider putting this into a function
-        if (is_agg_expr(left_col)) left_col <- sub_agg_column(left_col, env)
-        if (is_agg_expr(right_col)) right_col <- sub_agg_column(right_col, env)
-        cond <- pred_func(left_col, right_col)
-        call_method(cond@jc, "toString")
-      } else if (is_wndw_expr(left) | is_wndw_expr(right)) {
-
-        pred_func <- rlang::call_fn(dot)
-        args <- rlang::call_args(dot)
-        dot_env <- rlang::quo_get_env(dots[[i]])
-        quos <- rlang::as_quosures(args, env = dot_env)
-        left_col <- rlang::eval_tidy(quos[[1]], df_cols)
-        right_col <- rlang::eval_tidy(quos[[2]], df_cols)
-
-        if (is_wndw_expr(left)) left_col <- sub_wndw_column(left_col, env)
-        if (is_wndw_expr(right)) right_col <- sub_wndw_column(right_col, env)
-
-        cond <- pred_func(left_col, right_col)
-        call_method(cond@jc, "toString")
-      } else rlang::quo_text(dot)
-
-    }
-  }
-
   # get a list for columns
   conds <- list()
   # make a separate environment we can pass around the recursive function
@@ -397,12 +243,19 @@ filter.spark_tbl <- function(.data, ..., .preserve = FALSE) {
   .counter_env$to_drop <- character()
   .counter_env$sdf <- sdf
   .counter_env$j <- 0
+  .counter_env$df_cols <- df_cols
+  .counter_env$groups <- attr(.data, "groups")
+
   for (i in seq_along(dots)) {
     # because we are working with a recursive function I'm going to create a
-    # separate environemnt to keep all the counter vars in and just pass that along
-    # every time the recursive function is called
+    # separate environemnt to keep all the counter vars in and just pass that
+    # along every time the recursive function is called
     dot_env <- rlang::quo_get_env(dots[[i]])
-    quo_sub <- rlang::parse_quo(fix_dot(dots[[i]], .counter_env), env = dot_env)
+    .counter_env$orig_env <- dot_env
+    quo_sub <- rlang::parse_quo(
+      fix_dot(dots[[i]], .counter_env),
+      env = dot_env
+    )
     sdf <- .counter_env$sdf
 
     df_cols_update <- get_jc_cols(sdf)
