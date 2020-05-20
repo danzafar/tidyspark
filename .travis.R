969d5c5b3f32d858535d18bbbd2afaf068906ea2
S Daniel Zafar <57200935+danzafar@users.noreply.github.com>
2020-05-20 17:48:06 -0600

    Update .travis.R

--- .travis.R
+++ .travis.R
@@ -1,12 +1,4 @@
 parent_dir <- dir("../", full.names = TRUE)
 SparkR:::install.spark(overwrite = TRUE)
 
-cat("files in current dir:\n")
-print(list.files())
-
-cat("files in parent dir:\n")
-print(list.files(".."))
-
-install.packages("tidyspark_0.0.0.tar.gz", repos = NULL, type = "source")
-
 source("testthat.R")
