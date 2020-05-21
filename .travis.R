0c322c63c5ac870472b7272ac8d65382b6afca87
S Daniel Zafar <57200935+danzafar@users.noreply.github.com>
2020-05-21 15:47:13 -0600

    more debugs

--- .travis.R
+++ .travis.R
@@ -2,6 +2,7 @@ parent_dir <- dir("../", full.names = TRUE)
 SparkR:::install.spark(overwrite = TRUE)
 
 library(dplyr)
+install.packages("../tidyspark_0.0.0.tar.gz", repos = NULL, type="source")
 library(tidyspark)
 
 source("testthat.R")
